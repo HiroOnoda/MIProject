fb3a0905c2d93fb410d0f19ac8de8ea0dedbd51c
danil <danil@DESKTOP-SK7DR6D>
2019-10-08 00:34:11 +0300

    Добавил проверку пароля

--- MIPChat/DAL/Repository/UserRepository.cs
+++ MIPChat/DAL/Repository/UserRepository.cs
@@ -11,5 +11,19 @@ namespace MIPChat.DAL.Repository
         public UserRepository(ChatDBContext context) : base(context)
         {
         }
+
+        public Task<User> FindUserByEmail(string Email)
+        {
+            return _dbSet.FindAsync(Email);
+        }
+
+        public async bool PasswordCheck(LoginModel input)
+        {
+            User check = FindUserByEmail(input.Email);
+            if (check.RegisterModel.Password == input.Password)
+            {
+                return true;
+            }
+        }
     }
 }
